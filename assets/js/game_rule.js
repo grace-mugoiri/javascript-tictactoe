3ba71ced73de47d59ac78af45a5a972b926d0e13
grace-mugoiri <gracemugoiri@gmail.com>
2019-09-26 18:10:20 +0300

    refactor turn function to use factorial function

--- assets/js/game_rule.js
+++ assets/js/game_rule.js
@@ -1,12 +1,51 @@
-const gameRule = ((displayBoard) => {
-  const checkWin = (displayBoard, player) => {
+const gameRule = (() => {
+	const winCombos = Object.freeze([
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	]);
 
+  const checkWin = (board, player) => {
+		const plays = board.reduce((a, e, i) => ((e === player.piece) ? a.concat(i) : a), []);
+		console.log(plays, player);
+		let gameWon = null;
+		for (const [, win] of winCombos.entries()) {
+			if (win.every(element => plays.indexOf(element) > -1)) {
+				gameWon = { indexes: win, player };
+				console.log(gameWon);
+				break;
+			}
+		}
+		return gameWon;
   };
-  const turn = (positionId, player) => {
-    // put board with player.piece
-    const gameWon = checkWin(displayBoard, player);
+  const turn = (board, positionId, player) => {
+		// put board with player.piece
+		console.log(board);
+		board.updateBoard(positionId, player.piece)
+		uiController.displayPosition(positionId, player);
+    const gameWon = checkWin(board.board, player);
     if (gameWon) {
-      ui_controller.gameOver(gameWon);
-    }
-  };
+      uiController.gameOver(gameWon);
+		}
+
+	};
+
+	const checkTie = () => {
+		let gameTie = null;
+		if (board.emptySquares().length === 0) {
+			gameTie = { indexes: Array.from(Array(9).keys()) };
+		}
+		return gameTie;
+	};
+
+	return {turn}
 })();
+
+
+
+
--- assets/js/ui_controller.js
+++ assets/js/ui_controller.js
@@ -7,22 +7,56 @@ const uiController = (() => {
     displayBoard = gameBoard();
     humanPlayer = player('user', 'O');
     aiPlayer = player('Computer', 'X');
-  };
+	};
+	const colors = Object.freeze({
+		win : 'green',
+		tie : 'aqua',
+		lose : 'red',
+	});
+
+	const winStatus = Object.freeze({
+		win : 'You Win!',
+		tie : 'Tie Game!',
+		lose : 'You Lose!',
+	});
+
+	const cells = document.querySelectorAll('.cell');
 
   const resetDisplay = () => {
+
     // .endgame display none
     // each cell text empty
     // each cell background color remove
-  };
+	};
 
-  const turnClick = () => {
+	const bestSpot = () => {
+		return displayBoard.emptySquares()[0];
+	};
+
+  const turnClick = (square) => {
+		// if (typeof displayBoard[square.target.id] === 'number') {
+			// valid check?
+			gameRule.turn(displayBoard, square.target.id, humanPlayer);
+			console.log(square.target.id)
+		// };
     // valid?
-    // turn user
-    // // turn ai
-  };
+		// turn user
+
+		// turn ai
+		// turn(bestSpot(), aiPlayer);
+
+	};
+
+	const displayPosition = (squareId, {piece}) => {
+		document.getElementById(squareId).innerText = piece;
+
+	};
 
   const addClickForEachCell = () => {
-    // for each cell click add
+		// for each cell click add
+		for (let i=0; i < 9; i++){
+		cells[i].addEventListener('click', turnClick);
+		};
   };
 
   const gameOverState = (player) => {
@@ -35,14 +69,23 @@ const uiController = (() => {
       message = 'tie';
     }
     return message;
-  };
+	};
 
-  const declareWinner = () => {
 
+  const declareWinner = (who) => {
+		document.querySelector('.endgame .text').innerText = who;
+		document.querySelector('.endgame').style.display = "block";
   };
 
   const gameOver = ({ player, indexes }) => {
-    const state = gameOverState(player);
+		const message = gameOverState(player);
+		for (const index of game.indexes) {
+			document.getElementById(index).style.backgroundColor = colors[message];
+		}
+		for (let i = 0; i < cells.length; i++) {
+			cells[i].removeEventListener('click', turnClick);
+		}
+		declareWinner(winStatus);
     // backgroundColor setting
     // remove click
     // declareWinner
@@ -53,7 +96,9 @@ const uiController = (() => {
     // ask name
     resetDisplay();
     addClickForEachCell();
-  };
+	};
+	return {startGame, displayPosition};
+
 })();
 
 uiController.startGame();
