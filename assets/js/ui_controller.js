7c67b998e3f096ab7a41b2c48d9cd7320d1cb5f4
grace-mugoiri <gracemugoiri@gmail.com>
2019-09-27 13:50:57 +0300

    add name input functionality

--- assets/js/ui_controller.js
+++ assets/js/ui_controller.js
@@ -4,11 +4,11 @@ const uiController = (() => {
   let displayBoard = null;
   let humanPlayer = null;
   let aiPlayer = null;
-  // let playerName = null;
+  let playerName = null;
 
   const resetObject = () => {
     displayBoard = gameBoard();
-    humanPlayer = player('user', 'O');
+    humanPlayer = player(playerName, 'O');
     aiPlayer = player('Computer', 'X');
   };
   const colors = Object.freeze({
@@ -68,12 +68,13 @@ const uiController = (() => {
   };
 
 
-  const declareWinner = (who) => {
-    document.querySelector('.endgame .text').innerText = who;
+  const declareWinner = (player) => {
+		const innerText = player ? `${player.name} Win` :'Game Tie!';
+    document.querySelector('.endgame .text').innerText = innerText;
     document.querySelector('.endgame').style.display = 'block';
   };
 
-  const gameOver = ({ player, indexes }) => {
+  const gameOver = ({ indexes, player  }) => {
     const message = gameOverState(player);
     for (const index of indexes) {
       document.getElementById(index).style.backgroundColor = colors[message];
@@ -81,28 +82,37 @@ const uiController = (() => {
     for (let i = 0; i < cells.length; i++) {
       cells[i].removeEventListener('click', turnClick);
     }
-    declareWinner(winStatus[message]);
+    declareWinner(player);
   };
 
   const getName = () => {
+
     // reveal the box to ask name
     // if it is not reveal with popup, we should move board more below.
     // get name with button click
     // save name to user's name
-    const obj = document.querySelector('.name-input');
+    playerName = document.querySelector('.name-input').value;
     // console.log(obj.value);
     // save
     // winStatus(obj.value).win;
-    return obj.value;
-  };
+	};
+	const askNameStart = () => {
+		document.querySelector('.name-input-box').style.display = 'block'
+	};
+	const askNameEnd = () => {
+		document.querySelector('.name-input-box').style.display = 'none';
+		document.querySelector('.name-input').value = '';
+	};
 
   const startGame = () => {
+
     resetObject();
-    getName();
+		getName();
+		askNameEnd();
     resetDisplay();
     addClickForEachCell();
   };
-  return { startGame, displayPosition, gameOver };
+  return { startGame, displayPosition, gameOver, askNameStart };
 })();
 
-uiController.startGame();
+uiController.askNameStart();
